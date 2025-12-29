e7f9b01fcfe75a4cd0068a91ddb3e2521eee7445
Thiwanka-1 <128372987+Thiwanka-1@users.noreply.github.com>
2025-12-29 10:42:24 +0530

    added the root for the frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+// client/src/App.jsx
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import ChatPage from "./pages/ChatPage.jsx";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<ChatPage />} />
+      {/* later you can add more pages here, e.g.
+          <Route path="/settings" element={<SettingsPage />} />
+      */}
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
+}
+
+export default App;
